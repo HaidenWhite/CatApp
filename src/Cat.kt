1b5a99f745cc1f4fc5a30dd9e34a0efea37f0c8c
HaidenWhite <0whith69@solent.ac.uk>
2025-10-03 09:10:41 +0100

    Updated eat and walk methods to be boolean

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,16 +1,23 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    fun walk() {
-        weight--
+    public fun walk(distance: Int) : Boolean
+    {
+        if(weight - distance >= 5) {
+            weight -= distance
+            return true
+        }
+        return false
     }
 
-    fun eat() {
-        weight ++
+    fun eat() : Boolean {
+        if (weight < 20) {
+            weight ++
+            return true
+        }
+        return false
     }
 
     override fun toString() : String {
         return "Name: $name Age: $age Weight: $weight"
     }
 }
-
-// this is a change
